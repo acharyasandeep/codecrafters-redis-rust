5a4185939ae6884e08b5b4a0fbdd32c14d80d1c7
Sandeep Acharya <acharyas378@gmail.com>
2024-05-12 10:43:46 +0545

    First Stage - Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +14,20 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let mut buf_reader = BufReader::new(&_stream);
+                let mut buffer = vec![0; 100];
+                // let mut req = String::new();
+                // let _ = buf_reader.read_line(&mut req);
+                let _ = buf_reader.read(&mut buffer);
+                println!(
+                    "Request is {:?}",
+                    String::from_utf8_lossy(&buffer).to_string()
+                );
+                // println!("Request is {:?}", req);
+
+                let _ = _stream.write_all(b"+PONG\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
